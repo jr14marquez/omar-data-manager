4c1e4c7c40aeff144708096a2babee052d317feb
JR Marquez <Jr14marquez@gmail.com>
2018-06-18 21:37:36 -0400

    restructure project and add client ui

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand href="#">
+        <img id="o2-logo" src="https://omar-rel.ossim.io/omar-ui/assets/o2-logo-61e1cb4d9a73f21683c6d15c66ec48c7.png" alt="">
+          OMAR-DATA-MANAGER
+      </b-navbar-brand>
+    </b-navbar>
+    <!--<router-view></router-view>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#o2-logo {
+  height:32px;
+  width:32px;
+}
+</style>
